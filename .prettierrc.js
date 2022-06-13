366f8311bef3f9294473910430280c7f1c9d8cb7
SamWarden <SamWardenSad@gmail.com>
2022-06-13 18:54:04 +0300

    Add packages to make code prettier

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  overrides: [
+    {
+      files: "*.sol",
+      rules: {
+        "unit-case": null,
+      },
+      options: {
+        bracketSpacing: false,
+        printWidth: 300,
+        tabWidth: 4,
+        useTabs: false,
+        singleQuote: false,
+        explicitTypes: "always",
+      },
+    },
+    {
+      files: "*.ts",
+      options: {
+        printWidth: 145,
+        semi: false,
+        tabWidth: 4,
+        trailingComma: "es5",
+      },
+    },
+  ],
+};
