1526d31342aa77ac1c34f5510560337e6da0e4d1
SamWarden <SamWardenSad@gmail.com>
2022-06-13 19:04:10 +0300

    Add an example of an ERC20Mock deployment script

---
+++ deploy/ERC20Mock.ts
@@ -0,0 +1,26 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+
+module.exports = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts } = hre
+  const { deploy } = deployments
+
+  const { deployer } = await getNamedAccounts()
+  console.log('deployer:', deployer)
+  
+  const name = 'ERC20Mock'
+  const symbol = 'ERC'
+  const decimals = 18  
+
+  console.log('Deploying a contract with name = %s, symbol = %s, decimals = %s', name, symbol, decimals)
+  const erc20 = await deploy('ERC20Mock', {
+    args: [
+      name,
+      symbol,
+      decimals,
+    ],
+    from: deployer,
+    log: true,
+  })
+  console.log(erc20)
+}
+module.exports.tags = ["ERC20Mock"]
