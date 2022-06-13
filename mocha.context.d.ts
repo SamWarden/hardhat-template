713b3c458c6993c8321d41e21da5299ce57328c2
SamWarden <SamWardenSad@gmail.com>
2022-06-13 19:14:53 +0300

    Change indent spaces

--- mocha.context.d.ts
+++ mocha.context.d.ts
@@ -2,19 +2,19 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { ERC20Mock } from "./build/typechain"
 
 declare module "mocha" {
-    export interface Context {
-        // SIGNERS
-        signers: SignerWithAddress[]
-        owner: SignerWithAddress
-        alice: SignerWithAddress
-        bob: SignerWithAddress
-        carol: SignerWithAddress
-        tema: SignerWithAddress
-        misha: SignerWithAddress
+  export interface Context {
+    // SIGNERS
+    signers: SignerWithAddress[]
+    owner: SignerWithAddress
+    alice: SignerWithAddress
+    bob: SignerWithAddress
+    carol: SignerWithAddress
+    tema: SignerWithAddress
+    misha: SignerWithAddress
 
-        // CONTRACTS
-        token1: ERC20Mock
-        token2: ERC20Mock
-        token3: ERC20Mock
-    }
+    // CONTRACTS
+    token1: ERC20Mock
+    token2: ERC20Mock
+    token3: ERC20Mock
+  }
 }
