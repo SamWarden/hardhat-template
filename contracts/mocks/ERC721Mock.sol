77a5025f36a0163dae034356b2f8d7b248776556
SamWarden <SamWardenSad@gmail.com>
2022-06-24 22:26:19 +0300

    Add baseURI and contractURI to the ERC721Mock

--- contracts/mocks/ERC721Mock.sol
+++ contracts/mocks/ERC721Mock.sol
@@ -5,16 +5,24 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract ERC721Mock is ERC721, ERC721Enumerable, ERC721Burnable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    string public baseURI;
+    string public contractURI;
 
     constructor(
         string memory name,
-        string memory symbol
-    ) ERC721(name, symbol) {}
+        string memory symbol,
+        string memory baseURI_,
+        string memory contractURI_
+    ) ERC721(name, symbol) {
+        baseURI = baseURI_;
+        contractURI = contractURI_;
+    }
 
     function mintToken(address to) public {
         uint256 tokenId = _tokenIdCounter.current();
@@ -28,13 +36,13 @@ contract ERC721Mock is ERC721, ERC721Enumerable, ERC721Burnable {
         }
     }
 
-    // The following functions are overrides required by Solidity.
+    /**
+     * @dev See {IERC721Metadata-tokenURI}.
+     */
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token");
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
+        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, Strings.toString(tokenId), ".json")) : "";
     }
 
     function supportsInterface(bytes4 interfaceId)
@@ -44,5 +52,12 @@ contract ERC721Mock is ERC721, ERC721Enumerable, ERC721Burnable {
         returns (bool)
     {
         return super.supportsInterface(interfaceId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
     }
 }
