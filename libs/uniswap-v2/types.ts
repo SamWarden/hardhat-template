05ef2ac213958c89999f8b0d19f34de98bfd0964
SamWarden <SamWardenSad@gmail.com>
2022-07-17 01:04:48 +0300

    Update the uniswap-v2 lib

--- libs/uniswap-v2/types.ts
+++ libs/uniswap-v2/types.ts
@@ -1,4 +1,9 @@
-export type UniswapV2Addresses = {
+export type UniswapV2AddressesCollection = {
 	uniswapRouterAddresses: { [key: string]: string},
 	uniswapFactoryAddresses: { [key: string]: string},
+}
+
+export type UniswapV2Addresses = {
+	uniswapRouterAddress: string,
+	uniswapFactoryAddress: string,
 }
--- libs/uniswap-v2/utils.ts
+++ libs/uniswap-v2/utils.ts
@@ -1,11 +1,19 @@
 import fs from "fs"
-import type { UniswapV2Addresses } from "./types"
+import type { UniswapV2Addresses, UniswapV2AddressesCollection } from "./types"
 
-export function loadUniswapV2Addresses(): UniswapV2Addresses {
+export function loadUniswapV2Addresses(): UniswapV2AddressesCollection {
 	const {
 		UniswapV2Router02: uniswapRouterAddresses,
 		UniswapV2Factory: uniswapFactoryAddresses,
 	} = JSON.parse(fs.readFileSync('libs/uniswap-v2/addresses.json', {encoding: 'utf8'}))
 
 	return { uniswapRouterAddresses, uniswapFactoryAddresses }
+}
+
+export function loadUniswapV2AddressesForNetwork(network: string = "minnet"): UniswapV2Addresses {
+	const { uniswapRouterAddresses, uniswapFactoryAddresses } = loadUniswapV2Addresses()
+	const uniswapRouterAddress = uniswapRouterAddresses[network]
+	const uniswapFactoryAddress = uniswapFactoryAddresses[network]
+
+	return { uniswapRouterAddress, uniswapFactoryAddress }
 }
