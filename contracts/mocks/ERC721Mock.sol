d7637113e2102072a7f90e14089de2445f7fc7d6
SamWarden <SamWardenSad@gmail.com>
2022-11-18 15:57:16 +0300

    Fix ERC721Mock: upgrade to new openzeppelin version

--- contracts/mocks/ERC721Mock.sol
+++ contracts/mocks/ERC721Mock.sol
@@ -55,10 +55,13 @@ contract ERC721Mock is ERC721, ERC721Enumerable, ERC721Burnable {
         return super.supportsInterface(interfaceId);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
-        super._beforeTokenTransfer(from, to, tokenId);
+    
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 firstTokenId,
+        uint256 batchSize
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, firstTokenId, batchSize);
     }
 }
