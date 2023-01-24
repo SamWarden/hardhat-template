e65904a87a743225e5996fba988f351d5566f06e
SamWarden <SamWardenSad@gmail.com>
2023-01-25 01:51:11 +0300

    Refactor contracts mocks

--- contracts/mocks/ERC1155Mock.sol
+++ contracts/mocks/ERC1155Mock.sol
@@ -9,18 +9,18 @@ import {ERC1155Supply} from "@openzeppelin/contracts/token/ERC1155/extensions/ER
 contract ERC1155Mock is ERC1155, ERC1155Burnable, ERC1155Supply {
     constructor(string memory uri) ERC1155(uri) {}
 
-    function setURI(string memory newuri) public {
+    function setURI(string memory newuri) external {
         _setURI(newuri);
     }
 
     function mint(address account, uint256 id, uint256 amount, bytes memory data)
-        public
+        external
     {
         _mint(account, id, amount, data);
     }
 
     function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
-        public
+        external
     {
         _mintBatch(to, ids, amounts, data);
     }
