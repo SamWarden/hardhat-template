193b46aa71bcb74e29fa8d94db480292f1924abb
SamWarden <SamWardenSad@gmail.com>
2023-12-08 16:38:07 +0300

    feat(wallet): add specifying `number` and `json` for wallet generation

--- tasks/utils/generate-wallet.ts
+++ tasks/utils/generate-wallet.ts
@@ -1,10 +1,38 @@
-import { task } from "hardhat/config"
+import { task, types } from "hardhat/config"
+import { HardhatRuntimeEnvironment } from "hardhat/types"
 
+class Wallet {
+  constructor(
+    readonly address: string,
+    readonly privateKey: string,
+    readonly mnemonic: string,
+  ) {}
+}
+
+type Wallets = { [key: number]: Wallet }
+
+function genereateWallets(hre: HardhatRuntimeEnvironment, walletsNumber: number = 1): Wallets {
+  const wallets: Wallets = {}
+  for (let walletId = 1; walletId <= walletsNumber; walletId++) {
+    const wallet = hre.ethers.Wallet.createRandom()
+    wallets[walletId] = new Wallet(wallet.address, wallet.privateKey, wallet.mnemonic!.phrase)
+  }
+  return wallets
+}
 
 task("generate-wallet", "Generates a new wallet and prints its privateKey, address and mnemonic")
-  .setAction(async (_taskArgs, hre) => {
-    const wallet = hre.ethers.Wallet.createRandom()
-    console.log(`New wallet private key is ${wallet.privateKey}`)
-    console.log(`New wallet public address is ${wallet.address}`)
-    console.log(`New wallet mnemonic is ${JSON.stringify(wallet.mnemonic)}`)
+  .addParam("number", "Number of wallets to generate", 1, types.int)
+  .addFlag("json", "Prints the output in JSON format")
+  .setAction(async (taskArgs: { number: number, json: boolean }, hre: HardhatRuntimeEnvironment) => {
+    const wallets = genereateWallets(hre, taskArgs.number)
+    if (taskArgs.json) {
+      console.log(JSON.stringify(wallets, undefined, 2))
+      return
+    }
+    for (const [walletId, wallet] of Object.entries(wallets)) {
+      console.log(`Wallet ${walletId}:`);
+      console.log(`Public address: ${wallet.address}`);
+      console.log(`Private key: ${wallet.privateKey}`);
+      console.log(`Mnemonic: ${wallet.mnemonic}`);
+    }
   })
